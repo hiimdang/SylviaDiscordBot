6b60881d404840219ebc3ec2d75c6264130b38ed
JustAFlan <haidanglear@gmail.com>
2025-05-16 08:50:55 +0700

    Create defaults.py

---
+++ src/cogs/defaults.py
@@ -0,0 +1,7 @@
+default_params = {
+    "guild_ids": ["1059304552962199663"]  # Monarch Server ID
+}
+
+# default_ffmpeg_path = {
+#     "executable": "D:\\ffmpeg-7.1-full_build\\bin\\ffmpeg.exe" 
+# }
